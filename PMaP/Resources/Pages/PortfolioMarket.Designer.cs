3a1f1e78223af2870d6023f33b92d447df953380
Niño Allen Ardiente <ardientenino@gmail.com>
2022-01-11 19:07:25 +0800

    Include a plot in Portfolio market screen - some changes

--- PMaP/Resources/Pages/PortfolioMarket.Designer.cs
+++ PMaP/Resources/Pages/PortfolioMarket.Designer.cs
@@ -19,7 +19,7 @@ namespace PMaP.Resources.Pages {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class PortfolioMarket {
@@ -57,6 +57,15 @@ namespace PMaP.Resources.Pages {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Venta de Carteras de las diferentes entidades financieras (España).
+        /// </summary>
+        public static string ChartTitle {
+            get {
+                return ResourceManager.GetString("ChartTitle", resourceCulture);
             }
         }
         
